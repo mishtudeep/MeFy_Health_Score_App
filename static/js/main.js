73d7d02336d5d5dcd409e3d57772acfd44bcfe60
AcOdErKa <akar.bhisek@gmail.com>
2020-06-09 00:35:55 +0530

    Added validations

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,28 @@
+function checkAge(age){
+    if(age === ''){
+        alert("Fill provide a value");
+        return false;
+    } else if( age < 18){
+        alert("Age must be greater than 18");
+        return false;
+    } else if(!/^[0-9]+$/.test(age)){
+        alert("Enter Numeric Value only");
+        return false;
+    }
+    return true;
+}
+
+function checkField(fieldName){
+    if(fieldName === ''){
+        alert("Fill provide a value");
+        return false;
+    } else if($.isNumeric(fieldName) == false) {
+        alert("Please provide numeric value");
+        return false;
+    }
+    return true;
+}
+
 $(document).ready(function(){
     $("#loaderImg").hide();
     $("#notibadge").hide();
@@ -17,9 +42,15 @@ $("#submitbtn").click(function(e){
 	const alocholconsumptionweekly=$("input[name=alocholconsumptionweekly]").val();
     const smoker=$("input[name=smoker]:checked").val();
     
-    if(age === '' || gender=== '' || weight=== '' || height=== '' || heartrate=== '' || bloodpressuresys=== '' || bloodpressuredia=== '' || cholestrol=== '' || avgbloodsugar=== '' || alcoholconsumptiondaily=== '' || alocholconsumptionweekly=== '' || smoker=== ''){
-        alert("Please Enter Values in all the fields!!");
-    } else{
+    if(checkAge(age) === false || checkField(weight) === false || checkField(height) === false || checkField(heartrate) === false || checkField(bloodpressuresys) === false || checkField(bloodpressuredia) === false || checkField(cholestrol) === false || checkField(avgbloodsugar) === false || checkField(alcoholconsumptiondaily) === false || checkField(alocholconsumptionweekly) === false){
+        console.error("Fill all the values");
+        return;
+    }
+    if(gender === undefined || smoker === undefined){
+        alert("Fill all the values for gender / smoke");
+        return;
+    }
+    else{
         $.ajax({
             url: '/api/getScore',
             beforeSend: function(){
@@ -44,11 +75,26 @@ $("#submitbtn").click(function(e){
                 data = JSON.parse(data);
                 console.log(data['score']);
                 $("#healthScoreDisp").html(data['score']);
+                let scorecomment = ""
+                if(data['score'] >= 80){
+                    scorecomment = "You are doing very good"
+                    $("#scoreComment").html(scorecomment);
+                } else if(data['score'] >= 60 && data['score'] < 80){
+                    scorecomment = "Your health score is average";
+                    $("#scoreComment").html(scorecomment);
+                } else if(data['score'] >= 40 && data['score'] < 60) {
+                    scorecomment = "You are not doing well take care";
+                    $("#scoreComment").html(scorecomment);
+                }
+                else if(data['score'] < 40) {
+                    scorecomment = "Your health score is very low consult a doctor";
+                    $("#scoreComment").html(scorecomment);
+                }
                 console.log(data['recommendations']);
                 for(let i=0;i<data['recommendations'].length;i++) {
                     $("#pageSubmenu").append('<li><a href="#">'+data['recommendations'][i]+'</a></li>');
                 }
-                alert("Health Score Generated, please view it on the sidebar");
+                alert("Your Health Score is "+data['score']+", "+scorecomment+", for more details please view it on the sidebar");
             },
             complete:function(data){
                 $("#loaderImg").hide();
